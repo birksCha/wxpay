8dbec7d010e6e948971661b0f2610d18c4fa6196
Chi ZheHong <3238351902@qq.com>
2018-07-25 15:01:43 +0800

    init

---
+++ src/WxPay/Lib/Lib.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chi ZH
+ * Date: 2018/7/25
+ * Time: 14:58
+ */
+namespace birksCha\Lib;
+
+
+
+class Lily
+{
+    public function desc()
+    {
+        echo "this is lily flower";
+    }
+}
